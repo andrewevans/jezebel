64089c4f7f50979919340ffeaded692fedba091c
Andrew Evans <Andrew.Evans@babycenter.com>
2016-06-23 16:13:41 -0700

    Drag-and-drop shades to reassign their weights.
    Sorting was temporarily taken out in commit 8d72fcd405857bac86c6ed36db28270100afe6d0 . It is reintroduced in this commit.

--- app/components/user-shades-list.js
+++ app/components/user-shades-list.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  sortedShades: Ember.computed.sort('color.shades', 'sortDefinition'),
+  sortedShades: Ember.computed.sort('shades', 'sortDefinition'),
   sortDefinition: ['weight'],
+  actions: {
+    reorderItems(itemModels) {
+      // Reassign weights according to the order
+      // of their new index
+      itemModels.forEach(function (item, index) {
+        item.set('weight', index);
+      });
+    },
+  },
 });
