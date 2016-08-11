7e2cf514cd9c47da810a7d6bf44d9f97c78f3918
Andrew Evans <Andrew.Evans@babycenter.com>
2016-08-11 16:36:51 -0700

    List characters by weighted rank, highest to lowest.

--- app/components/characters-list.js
+++ app/components/characters-list.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  characters: null,
+  sorted_characters: Ember.computed.sort('characters', 'sort_definition'),
+  sort_definition: ['weight_ranked:desc'],
 });
