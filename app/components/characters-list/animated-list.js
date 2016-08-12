72799153fa3e4a708a96116f169fb2764bb334a2
Andrew Evans <Andrew.Evans@babycenter.com>
2016-08-11 20:49:18 -0700

    Pause music when animation is cancelled.

--- app/components/characters-list/animated-list.js
+++ app/components/characters-list/animated-list.js
@@ -3,14 +3,20 @@ import { task, timeout } from 'ember-concurrency';
 
 export default Ember.Component.extend({
   is_playing: false,
+  player: null,
   current_character_title: null,
   sorted_characters: Ember.computed.sort('characters', 'sort_definition'),
   sort_definition: ['weight_ranked:desc'],
+  didInsertElement() {
+    this._super(...arguments);
+
+    this.set('player', this.$('audio')[0]);
+  },
   cycleNames: task(function * () {
     var characters = this.get('sorted_characters'),
       character_length = characters.get('length'),
       count = character_length - 1,
-      player = this.$('audio')[0];
+      player = this.get('player');
 
     this.set('is_playing', true); // Flag that the cycle is currently playing
 
@@ -31,4 +37,11 @@ export default Ember.Component.extend({
       yield timeout((character.get('weight_ranked') * 1.1) + 10);
     }
   }).restartable(),
+  actions: {
+    cancelAll() {
+      this.get('player').pause();
+
+      this.get('cycleNames').cancelAll();
+    },
+  },
 });
