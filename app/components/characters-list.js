e89dd6cba8744331116e8f511f6895f75fc7674b
Andrew Evans <Andrew.Evans@babycenter.com>
2016-08-12 00:52:41 -0700

    Only show names in the animation that have a weight of more than zero.

--- app/components/characters-list.js
+++ app/components/characters-list.js
@@ -3,4 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   sorted_characters: Ember.computed.sort('characters', 'sort_definition'),
   sort_definition: ['weight_ranked:desc'],
+
+  // Filter out characters who have a weight of 0
+  filtered_characters: Ember.computed.filter('characters', function(character) {
+    if (character.get('weight_ranked') > 0) {
+      return true;
+    }
+  }),
 });
